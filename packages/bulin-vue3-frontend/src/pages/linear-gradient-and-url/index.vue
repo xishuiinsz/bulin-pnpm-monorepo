3afbb0e6db7a16d7c26e7e38f329eca37963a26b
xishuiofchina <xishuiofchina@163.com>
2025-08-19 22:36:13 +0800

    提交《背景图片和渐变色并存》demo

---
+++ packages/bulin-vue3-frontend/src/pages/linear-gradient-and-url/index.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import pageData from './page';
+</script>
+
+<template>
+  <div class="auto-generate-page w-100 h-100 d-flex flex-column">
+    <div class="crumbs">
+      <div class="el-breadcrumb" aria-label="Breadcrumb" role="navigation">
+        <span class="el-breadcrumb__item" aria-current="page" />
+        <span class="el-breadcrumb__inner" role="link">
+          <i class="el-icon-lx-warn" />
+          {{ pageData.title }}
+        </span>
+      </div>
+    </div>
+    <div class="container w-100 h-100 flex-fill">
+      <div class="login-container h-50" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.auto-generate-page {
+  .login-container {
+    width: 100%;
+    background: url("./img/bg2.png") no-repeat right bottom, linear-gradient(135deg, lightgreen 0, lightblue 100%);
+    background-size: contain;
+  }
+}
+</style>
---
+++ packages/bulin-vue3-frontend/src/pages/linear-gradient-and-url/page.js
@@ -0,0 +1,7 @@
+export default {
+  path: '/myComponents/linear-gradient-and-url',
+  title: '背景图片和渐变色并存',
+  hasChild: false,
+  order: 1,
+  component: () => import('./index.vue'),
+};
