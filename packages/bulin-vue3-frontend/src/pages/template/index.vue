4fa4e05264aa9625f3f791690b6dc413bac4260e
xishuiofchina <xishuiofchina@163.com>
2025-08-19 21:23:52 +0800

    提交《自动生成页面的模板》

---
+++ packages/bulin-vue3-frontend/src/pages/template/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import pageData from './page';
+</script>
+
+<template>
+  <div class="auto-generate-page w-100 h-100 d-flex flex-column">
+    <div class="crumbs">
+      <div class="el-breadcrumb" aria-label="Breadcrumb" role="navigation">
+        <span class="el-breadcrumb__item" aria-current="page" />
+        <span class="el-breadcrumb__inner" role="link">
+          <i class="el-icon-lx-warn" />
+          {{ pageData.title }}
+        </span>
+      </div>
+    </div>
+    <div class="container w-100 h-100 flex-fill">
+      <h4>{{ JSON.stringify(pageData, null, 4) }}</h4>
+      <div>如题所示，用来自动生成页面的模板</div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.auto-generate-page {}
+</style>
---
+++ packages/bulin-vue3-frontend/src/pages/template/page.js
@@ -0,0 +1,7 @@
+export default {
+  path: '/myComponents/auto-generate-page',
+  title: '自动生成页面的模板',
+  hasChild: false,
+  order: 1,
+  component: () => import('./index.vue'),
+};
