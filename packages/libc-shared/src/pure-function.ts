e889b2b0be643b48fce5bc7d3575dace572b725b
xishuiofchina <xishuiofchina@163.com>
2026-01-08 20:07:40 +0800

    Feat： 提交sortListByFeild纯函数

--- packages/libc-shared/src/pure-function.ts
+++ packages/libc-shared/src/pure-function.ts
@@ -33,3 +33,40 @@ export const getItemByList = <T extends Record<string, unknown>, K extends keyof
   return result;
 };
 
+/**
+ * @description list排序
+ *
+ * @example
+ * const result = sortListByFeild(
+ * [
+ *   { a: '-', b: 2, c: 3, d: 4 },
+ *   { a: 1, b: 2, c: 3, d: 4 },
+ *   { a: 10, b: 12, c: 3, d: 24 },
+ *   { a: 0, b: 22, c: -3, d: 49 },
+ *   { a: '-', b: -2, c: -93, d: 9 }
+ * ],
+ * { field: 'a', sort: 'asc' }
+ * );
+ */
+export const sortListByFeild = <T extends Record<string, number | string>>(
+  list: T[],
+  { field, sort }: { field: keyof T; sort: 'asc' | 'desc' }
+) => {
+  return list.sort((item1, item2) => {
+    const getValue = (num: number) => {
+      if (isNaN(num)) {
+        if (sort === 'asc') {
+          return Number.MAX_SAFE_INTEGER;
+        }
+        return -Number.MAX_SAFE_INTEGER;
+      }
+      return num;
+    };
+    const value1 = getValue(item1[field] as number);
+    const value2 = getValue(item2[field] as number);
+    if (sort === 'asc') {
+      return value1 - value2;
+    }
+    return value2 - value1;
+  });
+};
